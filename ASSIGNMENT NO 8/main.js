ef6062d16ff7c77032131d879b033ada52b7042d
ALYARA-MOBILE-ACCESSORIES <alyaramobileaccessories@gmail.com>
2024-04-29 11:54:47 +0500

    NINE ASSIGNMENT DONE

---
+++ ASSIGNMENT NO 8/main.js
@@ -0,0 +1,3 @@
+var favouriteNum = 100 / 20;
+var message = "HELLO , MY FAVOURITE NUMBER IS ".concat(favouriteNum);
+console.log(message);
---
+++ ASSIGNMENT NO 8/main.ts
@@ -0,0 +1,3 @@
+let favouriteNum = 100 / 20;
+let message = `HELLO , MY FAVOURITE NUMBER IS ${favouriteNum}`
+console.log(message);
