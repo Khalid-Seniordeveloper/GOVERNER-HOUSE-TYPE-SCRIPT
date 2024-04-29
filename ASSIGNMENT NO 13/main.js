515de37cf0f8461dc4bcae3da1b893adc01a2403
ALYARA-MOBILE-ACCESSORIES <alyaramobileaccessories@gmail.com>
2024-04-30 00:28:02 +0500

    THIRTEEN DONE

---
+++ ASSIGNMENT NO 13/main.js
@@ -0,0 +1,3 @@
+var transport = ["\n\n    \"I LOVE TO RIDE MOTORCYCLE\",\n    \"I  LOVE CARS ALSO\",\n    \"I LOVE BICYCLES\"\n\n"
+];
+console.log(transport);
---
+++ ASSIGNMENT NO 13/main.ts
@@ -0,0 +1,9 @@
+let transport = [`
+
+    "I LOVE TO RIDE MOTORCYCLE",
+    "I  LOVE CARS ALSO",
+    "I LOVE BICYCLES"
+
+`
+]
+console.log(transport);
