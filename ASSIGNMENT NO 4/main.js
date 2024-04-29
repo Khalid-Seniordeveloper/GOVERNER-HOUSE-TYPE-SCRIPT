1e66fbf38032469e6df04c145642c24b6196f310
ALYARA-MOBILE-ACCESSORIES <alyaramobileaccessories@gmail.com>
2024-04-29 11:29:33 +0500

    5TH DONE

---
+++ ASSIGNMENT NO 4/main.js
@@ -0,0 +1,4 @@
+var personName = "ALBERT EINSTYEIN";
+var quote = "A PERSON WHO NEVER MADE A MISTAKE NEVER TRIED ANYTHING NEW ";
+var message = "".concat(personName, "  once said ").concat(quote);
+console.log(message);
---
+++ ASSIGNMENT NO 4/main.ts
@@ -0,0 +1,5 @@
+let personName = "ALBERT EINSTYEIN"
+let quote = "A PERSON WHO NEVER MADE A MISTAKE NEVER TRIED ANYTHING NEW "
+let message = `${personName}  once said ${quote}`
+
+console.log(message);
