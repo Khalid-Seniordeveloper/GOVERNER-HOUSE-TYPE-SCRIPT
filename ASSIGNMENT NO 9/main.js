1d87e0f31558723f452e78858e7b37961d8365fc
ALYARA-MOBILE-ACCESSORIES <alyaramobileaccessories@gmail.com>
2024-04-29 11:57:33 +0500

    TENTH ASSIGNMENT DOME

---
+++ ASSIGNMENT NO 9/main.js
@@ -0,0 +1,3 @@
+// HELLO MY FREINDS TODAY I WANNA INTRODUCE YOU MY SESLF 
+var personName = "KHALID";
+console.log(personName);
---
+++ ASSIGNMENT NO 9/main.ts
@@ -0,0 +1,5 @@
+// HELLO MY FREINDS TODAY I WANNA INTRODUCE YOU MY SESLF 
+
+let personName = "KHALID";
+console.log(personName);
+ 
